19c6390503ad836c909c2e9826f27c7e01e0af1c
Mayerikson <44329039+Mayerikson@users.noreply.github.com>
2025-11-16 23:16:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.title("Dashboard de Análise de Comportamento de Compra")
-st.write("Bem-vindo ao dashboard interativo do projeto de varejo.")
 
-# Carregar dataset
-df = pd.read_csv("data/shopping_behavior_updated.csv")
+# Arquivo está na raiz do repo
+df = pd.read_csv("shopping_behavior_updated.csv")
+
 st.dataframe(df.head())
